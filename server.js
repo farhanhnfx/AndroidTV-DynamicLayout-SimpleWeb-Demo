db294724a3500ff31562f6313e51d328337ceaf2
Farhan Hanif <farhanhanif@mail.ugm.ac.id>
2026-05-17 22:11:18 +0700

    increase middleware express.json limit from 100kb to 5mb

--- server.js
+++ server.js
@@ -326,7 +326,8 @@ const path    = require('path');
 const Redis   = require('ioredis');
 
 const app = express();
-app.use(express.json());
+app.use(express.json({ limit: '5mb' })); 
+app.use(express.urlencoded({ limit: '5mb', extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use('/media', express.static(path.join(__dirname, 'media')));
 
